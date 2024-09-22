3c674f4dc8616cb29b244f5d69c6583c93f6e0f4
Richard Divino <richarddivino@Richards-Mini.lan>
2024-09-22 11:34:33 +0800

    Add name in copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="bg-zinc-50 dark:bg-zinc-800 h-24 flex items-center justify-center rounded-lg">
       <div className="text-sm text-zinc-900 dark:text-white ">
-        <p>&copy; {new Date().getFullYear()} Your Name. All Rights Reserved.</p>
+        <p>&copy; {new Date().getFullYear()} Richard Divino. All Rights Reserved.</p>
       </div>
     </div>
   )
